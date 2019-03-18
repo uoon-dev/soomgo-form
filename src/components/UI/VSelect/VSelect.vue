c417ac6472882bbb436fe2e51cc8852c5813f9f9
uoon <uoon.dev@gmail.com>
2019-03-18 14:51:33 +0900

    make v-select component

---
+++ src/components/UI/VSelect/VSelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <select
+    v-model="value"
+    @change="selectChanged"
+  >
+    <option v-for="option in options" :key="option.id" :value="option.text">
+      {{ option.text }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    selectChanged: {
+      type: Function,
+      default: () => {},
+    },
+  },
+};
+</script>
